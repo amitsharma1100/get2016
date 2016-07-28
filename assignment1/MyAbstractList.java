9578e8c2bc55423c4a87ad22f4bef1922dd62080
amitsharma1100 <sharma.amit1100@gmail.com>
2016-07-28 18:24:49 +0530

    Assignment 1 Submission

---
+++ assignment1/MyAbstractList.java
@@ -0,0 +1,15 @@
+package session1.assignment1;
+
+public abstract class MyAbstractList<P> {
+
+	public abstract boolean add(P p);
+	public abstract P get(int index);
+	public abstract int size();
+	public abstract boolean add(P p,int index);
+	public abstract boolean remove(P p);
+	public abstract boolean remove(int index);
+	public abstract void clear();
+	public abstract MyAbstractList<P> reverse();
+	public abstract MyAbstractList<P> sort();
+	public abstract MyAbstractList<P> concate(MyAbstractList<P> list2);
+}
