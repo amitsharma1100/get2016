349536051dc968b5b7146494e00379e84de3e1a5
amitsharma1100 <sharma.amit1100@gmail.com>
2016-08-02 18:32:35 +0530

    Assignment

---
+++ assignment1/Preorder.java
@@ -0,0 +1,18 @@
+package session3.assignment1;
+
+/**
+ * 
+ * @author Amit
+ *
+ */
+public class Preorder {
+	
+	//contains the preorder traversal of the tree
+	//this is to check the mirror of the tree
+	
+	static String preorder="";
+	 public Preorder() {
+		preorder="";
+	}
+
+}
