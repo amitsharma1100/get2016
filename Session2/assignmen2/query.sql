e9f2ffabe950cce6d7effdb263475b9e73006c6d
amitsharma1100 <sharma.amit1100@gmail.com>
2016-08-11 18:11:42 +0530

    Assignment 2 Submission

---
+++ Session2/assignmen2/query.sql
@@ -0,0 +1,9 @@
+USE LIS;
+
+SELECT book_issue.issue_dt AS ISSUE_DATE, book_issue.accession_no AS ACCESSION_NUMBER, members.member_nm AS MEMBER_NAME,
+IFNULL(book_return.return_dt," ") AS RETURN_DATE FROM book_issue 
+LEFT JOIN book_return ON book_issue.accession_no = book_return.accession_no 
+AND book_issue.member_id = book_return.member_id
+AND book_issue.issue_dt = book_return.issue_dt 
+LEFT JOIN members ON book_issue.member_id = members.member_id
+ORDER BY ISSUE_DATE, MEMBER_NAME;
