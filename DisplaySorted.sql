c8b2773e79b45cf8fc70552d1cb8ddaa692d7bd6
amitsharma1100 <sharma.amit1100@gmail.com>
2016-08-11 18:16:54 +0530

    Delete DisplaySorted.sql

--- DisplaySorted.sql
+++
@@ -1,4 +0,0 @@
-USE ECOMMERCE;
-
-SELECT c1.category_id,c1.category_nm,IFNULL(c2.category_nm,'Top Category') AS Parent_Category 
-FROM categories c1 LEFT JOIN categories c2 ON c1.parent_id=c2.category_id ORDER BY c1.category_nm;
