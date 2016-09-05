d90ae808181884ea5bb40627b1b6daf3df35725c
amitsharma1100 <sharma.amit1100@gmail.com>
2016-09-05 11:42:13 +0530

    Update session2-assignment2.js

--- Assignment2/session2-assignment2.js
+++ Assignment2/session2-assignment2.js
@@ -7,22 +7,72 @@ function funPattern() {
 	} else if (isNaN(tempVar)) {
 		alert("Enter only numbers");
 		return false;
-	} else if (tempVar < 0) {
-		alert("Negative numbers are not allowed");
+	} else if (tempVar < 1) {
+		alert("Numbers less than 1 are not allowed");
 		return false;
 	} else {
 		var rowNo = parseInt(tempVar);
-		var result = "";
-
-		for (var i = 0; i < rowNo; i++) {
-			for (var j = 0; j < rowNo - i; j++) {
-				result += "&nbsp;&nbsp;";
-			}
-			for (var k = 0; k <= i; k++) {
-				result += "*&nbsp; ";
-			}
-			result += "<br>";
-		}
-		document.getElementById("result").innerHTML = result;
+		funDoWhileLoop(rowNo);
 	}
+}
+
+function funForLoop(rowNo) {
+	var result = "";
+
+	for (var i = 0; i < rowNo; i++) {
+		for (var j = 0; j < rowNo - i; j++) {
+			result += "&nbsp;&nbsp;";
+		}
+		for (var k = 0; k <= i; k++) {
+			result += "*&nbsp; ";
+		}
+		result += "<br>";
+	}
+	document.getElementById("result").innerHTML = result;
+	return true;
+
+}
+
+function funWhileLoop(rowNo) {
+	var result = "";
+	var i = 0;
+	while (i < rowNo) {
+		var j = 0;
+		while (j < rowNo - i) {
+			result += "&nbsp;&nbsp;";
+			j++;
+		}
+		var k = 0;
+		while (k <= i) {
+			result += "*&nbsp; ";
+			k++;
+		}
+		result += "<br>";
+		i++;
+	}
+	document.getElementById("result").innerHTML = result;
+	return true;
+
+}
+
+function funDoWhileLoop(rowNo) {
+	var result = "";
+	var i = 0;
+	do {
+		var j = 0;
+		while (j < rowNo - i) {
+			result += "&nbsp;&nbsp;";
+			j++;
+		}
+		var k = 0;
+		while (k <= i) {
+			result += "*&nbsp; ";
+			k++;
+		}
+		result += "<br>";
+		i++;
+	} while (i < rowNo);
+	document.getElementById("result").innerHTML = result;
+	return true;
+
 }
